b7b390c783c054c2f1b4e4ba66b3422ea64690e1
Ross A. Baker <ross@rossabaker.com>
2019-06-19 15:57:27 -0400

    http4s-0.21.x and friends

---
+++ core/src/main/scala-2.11-2.12/org/http4s/client/jdkhttpclient/compat.scala
@@ -0,0 +1,5 @@
+package org.http4s.client.jdkhttpclient
+
+private[jdkhttpclient] object compat {
+  val CollectionConverters = scala.collection.JavaConverters
+}
---
+++ core/src/main/scala-2.13/org/http4s/client/jdkhttpclient/compat.scala
@@ -0,0 +1,6 @@
+package org.http4s.client.jdkhttpclient
+
+private[jdkhttpclient] object compat {
+  val CollectionConverters = scala.jdk.CollectionConverters
+}
+
