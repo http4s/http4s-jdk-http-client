6231460b7f4d3ecaf4a01424aeed5e39b491cd72
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-08-19 11:03:09 +0200

    Update scalafmt-core to 3.0.0 (#478)
    * Update scalafmt-core to 3.0.0
    
    * Reformat with scalafmt 3.0.0

--- core/src/main/scala/org/http4s/jdkhttpclient/AlwaysCancelingSubscriber.scala
+++ core/src/main/scala/org/http4s/jdkhttpclient/AlwaysCancelingSubscriber.scala
@@ -18,11 +18,10 @@ package org.http4s.jdkhttpclient
 
 import java.util.concurrent.Flow
 
-/** An implementation of a JRE `Flow.Subscriber` which immediately cancels the
-  * `Flow.Subscription`.
+/** An implementation of a JRE `Flow.Subscriber` which immediately cancels the `Flow.Subscription`.
   *
-  * We use this to clean up resources and conform the JRE API contract in
-  * cases where the HTTP body is not read.
+  * We use this to clean up resources and conform the JRE API contract in cases where the HTTP body
+  * is not read.
   */
 private[jdkhttpclient] final class AlwaysCancelingSubscriber[A] extends Flow.Subscriber[A] {
   override def onSubscribe(subscription: Flow.Subscription): Unit = subscription.cancel
