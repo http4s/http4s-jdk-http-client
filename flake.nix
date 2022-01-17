5a98fdd32b4d79d5daa6a27ed54be7e085964bc2
amesgen <amesgen@amesgen.de>
2022-01-17 01:29:38 +0100

    Use typelevel-nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    typelevel-nix.url = "github:rossabaker/typelevel-nix";
+    nixpkgs.follows = "typelevel-nix/nixpkgs";
+    flake-utils.follows = "typelevel-nix/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlay ];
+        };
+      in
+      {
+        devShell = pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          typelevelShell = {
+            jdk.package = pkgs.jdk11;
+          };
+        };
+      }
+    );
+}
